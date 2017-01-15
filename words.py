8555af695aebed1e65d1d351599c35a693b1a9a7
jesusropi <jesusropi@gmail.com>
2017-01-15 10:20:02 +0100

    Words: get_html, get_translation and get_words

--- words.py
+++ words.py
@@ -2,28 +2,46 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
+from requests import get
 
-
-OUTPUT_DIRECTORY = 'output'
+CARDS_FOLDER = 'cards'
+SOUNDS_FOLDER = 'sounds'
 TRANSLATE_TO = 'uk'
 URL_WORD = 'http://www.wordreference.com/es/translation.asp?tranword='
 URL_SOUND = ('http://www.wordreference.com'
              '/audio/en/{}/general/'.format(TRANSLATE_TO))
+WORDS_FILE = 'words.txt'
 
 
 def get_words():
     """Lee del fichero ./vocabulario/words.txt de las palabras a traducir"""
-    return None
+    words = []
+    f = open(WORDS_FILE, 'r')
+    for l in f:
+        words.append(l.replace('\n', ''))
+    return words
 
 
 def get_pronountiation():
-    """Devuelve el mp3 solicitado"""
+    """Devuelve el mp3 solicitado: /audio/en/uk/general/"""
+    # Se puede hacer con una regex: /audio/en/uk/general/numero.mp3
     return None
 
 
-def get_translation():
+def get_sound_name(html):
+    return False
+
+
+def get_html(word):
+    html = get(URL_WORD + '{}'.format(word))
+    return html
+
+
+def get_translation(html):
     """Devuelve la traduccion solicitada"""
-    return None
+    soup = BeautifulSoup(html.text, 'html.parser')
+    trans = soup.find_all("td", "ToWrd")
+    return [t.next for t in trans[1:6]]
 
 
 def create_card(translation, sound):
@@ -35,10 +53,14 @@ def create_massive_import(cards):
     """Recibe una lista de cards y crea el fichero cards_words.txt
     preparado para cargar en Anki
     """
+    pass
 
 
 def main():
-    return None
+    html = get_html('word')
+    print get_translation(html)
+    print get_sound_name(html)
+    # get_pronountiation(url)
 
 
 if __name__ == '__main__':
